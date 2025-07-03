6e7c7c54a2b014267f44b59b8521cdd763386d9e
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-07-03 15:00:46 -0700

    Added initial keybinds and controller object to RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,25 @@
 package frc.robot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Turret;
+
+import frc.robot.commands.TurretJogCmd;
+import frc.robot.commands.TurretZeroOverrideCmd;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.TurretSetpointCmd;
 
 public class RobotContainer {
-  //Subsystems
+  private final CommandXboxController operatorController = new CommandXboxController(Constants.OPERATOR_CONTROLLER_PORT);
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    //Keybinds
+        operatorController.povUp().whileTrue(new TurretJogCmd(Turret.getInstance(), () -> -operatorController.getRightY() * Constants.JOYSTICK_JOG_SPEED_MULTIPLIER));
+        operatorController.leftBumper().onTrue(new InstantCommand(Turret.getInstance()::HotRefreshTurretConfig));
+        operatorController.povDown().onTrue(new TurretZeroOverrideCmd(Turret.getInstance()));
+
+        operatorController.y().whileTrue(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_TRIAL_SETPOINT));
+        operatorController.y().onFalse(new TurretSetpointCmd(Turret.getInstance(), Constants.ABSOLUTE_ZERO));
   }
 }
