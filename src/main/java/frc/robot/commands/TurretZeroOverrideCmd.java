35d198fca404697f1ba0fc9495acaf7fb40528bb
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-07-03 15:00:33 -0700

    Added TurretZeroOverrideCmd.java

--- src/main/java/frc/robot/commands/TurretZeroOverrideCmd.java
+++ src/main/java/frc/robot/commands/TurretZeroOverrideCmd.java
@@ -1,5 +1,37 @@
 package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Turret;
 
-public class TurretZeroOverrideCmd {
-    
+public class TurretZeroOverrideCmd extends Command {
+    private final Turret turret;
+
+    public TurretZeroOverrideCmd(Turret turret) {
+        this.turret = Turret.getInstance(); 
+        addRequirements(turret); 
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("TurretZeroOverrideCmd Online");
+    }
+
+    @Override
+    public void execute() {
+        turret.zeroTurret();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("TurretZeroOverrideCmd Offline");
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (turret.getTurretMotorEncoder() == Constants.ABSOLUTE_ZERO) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
