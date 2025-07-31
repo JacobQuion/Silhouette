94ef18d9deca4eb02069594f9b98fbc2e5b3f66a
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-07-31 13:46:12 -0700

    Added intermediate setpoints to Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,23 +6,25 @@ public class Constants {
     
     //====================Controller====================
     public static final int OPERATOR_CONTROLLER_PORT = 0;
-    public static final double JOYSTICK_JOG_SPEED_MULTIPLIER = 0.035;
+    public static final double JOYSTICK_JOG_SPEED_MULTIPLIER = 0.1; //0.035
 
     //====================Arm====================
     public static final int ARM_MOTOR_ID = 10;
 
-    public static final double ARM_kP = 25.0; //20.0
+    public static final double ARM_kP = 10.0; //20.0
     public static final double ARM_kI = 0.0; //0.05
     public static final double ARM_kD = 0.0; //0.7
-    public static final double ARM_kG = 0.0325; //0.325
+    public static final double ARM_kG = 0.3; //0.5
     public static final double ARM_kVelo = 1.0; //1.0
     public static final double ARM_kAccel = 2.0; //2.0
     public static final double ARM_CURRENT_LIMIT = 40.0;
-    public static final double ARM_SETPOINT_TOLERANCE = 0.025;
+    public static final double ARM_SETPOINT_TOLERANCE = 0.03;
  
-    public static final double ARM_ZERO_SETPOINT = 0.06; //0.0
-    public static final double ARM_INTERMEDIATE_SETPOINT = 0.12;//0.06
-    public static final double ARM_TIP_UP_SETPOINT = 0.2; //0.12
+    public static final double ARM_ZERO_SETPOINT = 0.075; //0.0
+    public static final double ARM_SETPOINT_1 = 0.14;//0.06
+    public static final double ARM_SETPOINT_2 = 0.19; //0.12
+    public static final double ARM_SETPOINT_3 = 0.25; //0.12
+    public static final double ARM_SETPOINT_4 = 0.3; //0.12
 
     //====================Turret====================
     public static final int TURRET_MOTOR_ID = 20;
@@ -37,6 +39,8 @@ public class Constants {
     public static final double TURRET_SETPOINT_TOLERANCE = 0.05;
 
     public static final double TURRET_ZERO_SETPOINT = 0.0;
-    public static final double TURRET_RIGHT_INTERMEDIATE_SETPOINT = 0.5;
-    public static final double TURRET_LEFT_INTERMEDIATE_SETPOINT = -0.5; //-0.1
+    public static final double TURRET_SETPOINT_1 = 0.25;
+    public static final double TURRET_SETPOINT_2 = 0.5;
+    public static final double TURRET_SETPOINT_3 = -0.25;
+    public static final double TURRET_SETPOINT_4 = -0.5;
 }
