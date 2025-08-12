29bdeb3123bdd6c4b383552eb5a900c582ba0fb9
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-08-12 13:23:19 -0700

    Updated Controller Bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,19 +2,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Turret;
+import frc.robot.commands.ArmFollowerCmd;
 import frc.robot.commands.ArmJogCmd;
-import frc.robot.commands.ArmPendulumCmd;
+import frc.robot.commands.ArmLoopCmd;
 import frc.robot.commands.ArmSetpointCmd;
-import frc.robot.commands.ArmTrajectoryCmd;
 import frc.robot.commands.ArmZeroOverrideCmd;
-import frc.robot.commands.ComboCmd;
-import frc.robot.commands.ProxyCmd;
+import frc.robot.commands.TurretFollowerCmd;
 import frc.robot.commands.TurretJogCmd;
-import frc.robot.commands.TurretPendulumCmd;
+import frc.robot.commands.TurretLoopCmd;
 import frc.robot.commands.TurretZeroOverrideCmd;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.TurretSetpointCmd;
-import frc.robot.commands.TurretTrajectoryCmd;
 
 public class RobotContainer {
   private final CommandXboxController operatorController = new CommandXboxController(Constants.OPERATOR_CONTROLLER_PORT);
@@ -24,25 +22,24 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-        //operatorController.back().onTrue(new ComboCmd(Turret.getInstance(), Arm.getInstance()));
-
-        //====================Arm Controller Bindings (Triggers + Bumpers)====================
-        operatorController.leftBumper().onTrue(new InstantCommand(Arm.getInstance()::HotRefreshArmConfig));
-
-        operatorController.leftTrigger().whileTrue(new ArmSetpointCmd(Arm.getInstance(), 30.0));
+        //====================Arm Subsystem====================
+        operatorController.leftTrigger().whileTrue(new ArmSetpointCmd(Arm.getInstance(), Constants.ARM_SETPOINT_B));
         operatorController.leftTrigger().onFalse(new ArmSetpointCmd(Arm.getInstance(), Constants.ARM_ZERO_SETPOINT));
 
-        operatorController.leftStick().onTrue(new ArmPendulumCmd(Arm.getInstance()));
+        operatorController.leftBumper().whileTrue(new ArmSetpointCmd(Arm.getInstance(), Constants.ARM_SETPOINT_C));
+        operatorController.leftBumper().onFalse(new ArmSetpointCmd(Arm.getInstance(), Constants.ARM_ZERO_SETPOINT));
 
-        //====================Turret Controller Bindings (ABXY, Start, Back)====================
-        //operatorController.rightBumper().onTrue(new InstantCommand(Turret.getInstance()::HotRefreshTurretConfig));
-
-        operatorController.rightTrigger().whileTrue(new TurretSetpointCmd(Turret.getInstance(), 90.0));
+        //====================Turret Subsystem====================
+        operatorController.rightTrigger().whileTrue(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_SETPOINT_A));
         operatorController.rightTrigger().onFalse(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_ZERO_SETPOINT));
 
-        operatorController.b().whileTrue(new TurretSetpointCmd(Turret.getInstance(), -90.0));
-        operatorController.b().onFalse(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_ZERO_SETPOINT));
-
-        operatorController.rightStick().onTrue(new TurretPendulumCmd(Turret.getInstance()));
+        operatorController.rightBumper().whileTrue(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_SETPOINT_E));
+        operatorController.rightBumper().onFalse(new TurretSetpointCmd(Turret.getInstance(), Constants.TURRET_ZERO_SETPOINT));
+  
+        //====================Data Collection====================
+        operatorController.a().onTrue(new ArmLoopCmd(Arm.getInstance(), Turret.getInstance()));
+        operatorController.b().onTrue(new TurretLoopCmd(Arm.getInstance(), Turret.getInstance()));
+        operatorController.x().onTrue(new ArmFollowerCmd(Arm.getInstance(), Turret.getInstance()));
+        operatorController.y().onTrue(new TurretFollowerCmd(Arm.getInstance(), Turret.getInstance()));
   }
 }
